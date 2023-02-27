654e670c1a2c64eb682732067a84359187ffc439
Manflo27 <maflores1987@yahoo.com>
2023-02-27 06:40:58 -0800

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 2: Coding Practical Questions with Jest
 
+
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -14,16 +16,17 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const numbersArray1 = [6, 7, 8, 9, 10]
-// Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
-// Expected output: [72, 81, 90, 99, 108]
 
-// b) Create the function that makes the test pass.
+// const numbersArray1 = [6, 7, 8, 9, 10]
+// // Expected output: [18, 21, 24, 27, 30]
+// const numbersArray2 = [24, 27, 30, 33, 36]
+// // Expected output: [72, 81, 90, 99, 108]
 
-// --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
+// // b) Create the function that makes the test pass.
 
-// a) Create a test with expect statements for each of the variables provided.
+// // --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
+
+// // a) Create a test with expect statements for each of the variables provided.
 
 const object1 = { number: 15 }
 // Expected output: "15 is divisible by three"
@@ -32,16 +35,73 @@ const object2 = { number: 0 }
 const object3 = { number: -7 }
 // Expected output: "-7 is not divisible by three"
 
-// b) Create the function that makes the test pass.
 
-// --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
+describe("divide3", () => {
+    it("returns if a number divisible by three", () => {
+      expect(divide3(object1.number)).toEqual("15 is divisible by three")
+      expect(divide3(object2.number)).toEqual("0 is divisible by three")
+      expect(divide3(object3.number)).toEqual("-7 is not divisible by three")
+    })
+  })
+//   ReferenceError: divide3 is not defined 
 
-// a) Create a test with expect statements for each of the variables provided.
+// //  Pseudocode: Create a function called divide3
+// // input takes number as an argument.
+// conditional to check if number is divisible by three using modulo symbol
+// Return is the number in string interpolation 
+// // output the number inside it is evenly divisible by three or not.
+  
+
+const divide3 = (number) => {
+if (number % 3===0)
+    return `${number} is divisible by three`
+    else if (number % 3===0)
+    return  `${number} is divisible by three`  
+    else if (number % 0!==3)
+    return  `${number} is not divisible by three`
+    }
+    
+
+
+    
+    
+
+// // b) Create the function that makes the test pass.
+
+// // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
+
+// // a) Create a test with expect statements for each of the variables provided.
 
 const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
 const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+describe("myNouns", () => {
+it("returns an array with the first word capitalized.", () => {
+expect(myNouns(randomNouns1)).toEqual("Streetlamp,Potato,Teeth,Conclusion,Nephew")
+ expect(myNouns(randomNouns2)).toEqual("Temperature,Database,Chopsticks,Mango")
+})})
+
+    //   ReferenceError: upperCase is not defined
+
+// Pseudocode: 1.create a function called myNouns
+// 2. input - iterate through the array using .map
+// 3. Uses chartAt to select which index is going to be capitalized,touppercase to uoopercase
+// first letter. .substring extracts the characters from start to end, 1 is where it stops.
+// 4 .join to convert the array to a string, seperates the commas.
+ 
+    const myNouns = (array) => {
+        return array.map(value => {
+        return value.charAt(0).toUpperCase(0) + value.substring(1)
+        }).join()
+        }
+            
+    // Expected: 1
+    // 
+
+
+        
+        
 
 // b) Create the function that makes the test pass.
 
@@ -57,3 +117,28 @@ const vowelTester3 = "challenges"
 // Expected output: 2
 
 // b) Create the function that makes the test pass.
+describe("myIndex", () => {
+    it("returns the index of the first vowel.", () => {
+    expect(myIndex(vowelTester1)).toEqual(1)
+    expect(myIndex(vowelTester2)).toEqual(0)
+     expect(myIndex(vowelTester3)).toEqual(2)
+        })
+      })
+    //   ReferenceError: myIndex is not defined
+    //  Pseudocode: 1. Create a Function called myIndex
+    //  For loop to iterate through array of myVowel
+    //  return string if it includes a,e,i,o,u. Im stuck at this point, .indexOf is supposed to return 
+    // the first index found in the array.
+
+    const myIndex = (string) => {
+    let myVowel = ["a,e,i,o,u"];
+  for (let i = 0; i < myVowel.length; i++) {
+    if (string.includes[i]){
+        return string.indexOf()
+    }
+  }}
+
+//   Expected: 1
+//   Received: undefined
+
+    // Tests:       1 failed, 2 passed, 3 total
--- coding-concepts.js
+++ coding-concepts.js
@@ -8,36 +8,39 @@
 // --------------------1) What will this log?
 
 const cohort = "Alpha 2023"
-// console.log(cohort.split(""))
+console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: I believe it will log each character in that string individually as it is splitting up the string..split turns a a string into an array of those chracters.
+// // b) Verify and explain: 'A', 'l', 'p', 'h','a', ' ', '2', '0','2', '3' was the output. Each character was split and logged out individually.
 
 // --------------------2) What will this log?
 
 const greeter = (name) => {
-  `Hello, ${name}!`
+   `Hello, ${name}!`
 }
-// console.log(greeter("LEARN Student"))
+console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This will log out "Hello Learn Student!" as the function of greeter is passing a parameter of name and returning Hello with string interprolation to make it dynamic.
+// b) Verify and explain: It came back undefined, I believe by adding a return on line 19 would invoke the correct output of Hello, LEARN Student!
 
 // --------------------3) What will this log?
 
 const multipliedByTwo = [4, 5, 6, 7, 8].map((number) => number * 2)
-// console.log(multipliedByTwo)
+console.log(multipliedByTwo)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: multipliedByTwo is equal to an arrray of numbers.By using .map it is using iteration to go through the array. 
+// The arrow function allows the array to return the number multiplied by 2. I believe the number should be value as it is always the first that is the output.
+// b) Verify and explain: [ 8, 10, 12, 14, 16 ] was the output. The .map iterated through original array to output the sam elength of the array but with each one multiplied by 2. 
+// The arrow syntax returned the array with each element multiplied by 2.
 
 // --------------------4) What will this log?
 
 const onlyOdds = [11, 12, 13, 14, 15].filter((number) => number % 2 !== 0)
-// console.log(onlyOdds)
+console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:Thge output should be [11,13,15] as the .filter iterates through the array and return the stated condition. the stated condition
+// in this case is modulo of 2 !==0 which means return the odd numbers in that array
+// b) Verify and explain: [ 11, 13, 15 ] was the output. My original answer was correct as the array was filtered to produce only odd numbers in the original array.
 
 // --------------------5) What will this log?
 
@@ -47,7 +50,10 @@ const myCodingSkills = {
   databases: "PostgreSQL",
   versionControl: "GitHub"
 }
-// console.log(myCodingSkills.languages[0])
+console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Listed are objects that contain strings. The expected output should be "JavaScript" as it is nested in the key value pairs of languages. The [0] is the
+// index of the position in that array in languages.
+// b) Verify and explain: JavaScript was the output. I was right in my initial answer as it is nested in the the key value pairs. By 
+// console logging (myCodingSkills.languages[0]) it takes the position in that array and outputs it. 
+ 
